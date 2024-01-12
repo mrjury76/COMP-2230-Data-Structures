6bb6d90b49ff3b79793d23d5cffef82b76db9561
mrjury76 <mrjury76@gmail.com>
2024-01-11 17:20:06 -0800

    Refactor question7.java: Add missing method and remove unused code

--- Assignment 1/question7.java
+++ Assignment 1/question7.java
@@ -1,11 +1,27 @@
 public class question7 {
     public static void main(String[] args) {
+        logN();
+        constant();
+        nLogN();
+    }
+
+    private static void logN() {
         int count = 0;
+
         for(int i = 100; i > 0; i/=2){
             System.out.println("Hello World");
             // System.out.println(i);
             count++;
         }
-        System.out.println(count);   
+
+        System.out.println(count); 
+    }
+
+    private static void constant() {
+        System.out.println("Hello World");
+    }
+
+    private static void nLogN() {
+        //idk
     }
 }

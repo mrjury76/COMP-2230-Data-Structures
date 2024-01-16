f34837fe91b7dad5425cd65c1f4867469c7a7eea
mrjury76 <mrjury76@gmail.com>
2024-01-16 13:32:12 -0800

    finished assignment questions

--- Assignment 1/question7.java
+++ Assignment 1/question7.java
@@ -3,6 +3,7 @@ public class question7 {
         logN();
         constant();
         nLogN();
+        n2();
     }
 
     private static void logN() {
@@ -23,5 +24,27 @@ public class question7 {
 
     private static void nLogN() {
         //idk
+    }
+
+    private static void n2() {
+        int[] arr = {10,43,23,67,54,3,21,54,67};
+        // Initialize the array if necessary
+    
+        // Bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {  //
+            for (int j = 0; j < arr.length - 1 - i; j++) {  
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    
+        // Print the sorted array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }

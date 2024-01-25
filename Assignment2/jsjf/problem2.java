ff5b113b62f5d6d800a8e83c19493fba7d4caf82
mrjury76 <mrjury76@gmail.com>
2024-01-25 13:56:51 -0800

    finalizing the drop out stack output program

--- Assignment2/jsjf/problem2.java
+++ Assignment2/jsjf/problem2.java
@@ -8,14 +8,29 @@ package jsjf;
 
 public class problem2 {
     public static void main(String[] args)  {
-        DropOutArrayStack<Integer> array = new DropOutArrayStack<Integer>(10);
 
+        DropOutArrayStack<Integer> dropArray = new DropOutArrayStack<Integer>(12);
+        
+        System.out.println("Is Empty: " + dropArray.isEmpty()); //testing the drop array.
+        
+        for(int i = 0; i < dropArray.getLength(); i++){ //pushing a .length amount on numbers onto the stack to test is it wraps around.
+            dropArray.push(i+1);
+        }
 
-        System.out.println(array.size());
-        array.push(12);
-        System.out.println();
-        System.out.println(array.peek());
-        array.printStackElements();
-        array.toString();
+        dropArray.push(100);  //pushing the wrapped elements
+        dropArray.push(101);
+        dropArray.push(102);
+        dropArray.push(103);
+        System.out.println("Peek: " + dropArray.peek());  //testing the peek method
+        System.out.println("Pop: " + dropArray.pop());  //testing the pop method
+        dropArray.push(104);
+        dropArray.pop();
+        // dropArray.pop();
+        // dropArray.pop();
+        // dropArray.pop();
+        System.out.println(dropArray.isEmpty());
+        System.out.println(dropArray.toString());   //printing the full stack using toString method
+        System.out.println("Size(top): " + dropArray.size());  //printing size using top variable
+        System.out.println("Array Length: " + dropArray.getLength());  //printing actual array length
     }
 }

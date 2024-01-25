9bd8ac4ace41d8ac41ccef7d330fc448daa06817
mrjury76 <mrjury76@gmail.com>
2024-01-24 17:03:27 -0800

    final got the drop out logic worked out

--- Assignment2/jsjf/DropOutArrayStack.java
+++ Assignment2/jsjf/DropOutArrayStack.java
@@ -10,29 +10,26 @@ public class DropOutArrayStack<T> extends ArrayStack<T> {
         super(initialCapacity); // Calls the parent class's constructor with an initial capacity
     }
 
-    @Override
-    public void push(T element) {
-        if (size() == stack.length) 
-            top = top % stack.length; 
-
-        stack[top] = element;
-        top++;
+    //pushes element onto stack using a drop out mechanism
+    public void push(T element) { 
+        int wrappedIndex = (top % stack.length); //assigns the modulus of top and stack length to wrappedIndex
+        stack[wrappedIndex] = element; //pushes elements onto the stack using the wrapped index
+        top++;  //increments top
     }
 
-    @Override
+    //overrides parent's toString to print out full stack
     public String toString() {
-        StringBuilder output = new StringBuilder();
+        StringBuilder output = new StringBuilder("[");
 
-        // Include additional debugging information
-        output.append("Stack size = ").append(size()).append(": ");
-
-        // Traverse the stack to print elements
-        for (int i = 0; i < size(); i++) {
-            if (i > 0) {
+        for(int i = 0; i < stack.length; i++) {
+            if(i < top) {
+                output.append(stack[i]);
+            }
+            if(i < stack.length-1) {
                 output.append(", ");
             }
-            output.append(stack[(top - size() + i + stack.length) % stack.length]);
         }
+        output.append("]");
 
         return output.toString();
     }

39e62fcffc7829d40755da064af6fa8a056aed2b
mrjury76 <mrjury76@gmail.com>
2024-02-05 14:48:29 -0800

    added the ordered linked list class to add the "add" function

---
+++ Assignment2/jsjf/LinkedList/OrderedLinkedList.java
@@ -0,0 +1,24 @@
+package jsjf.LinkedList;
+
+
+/**
+ * This is my linked list child, ordered linked list.
+ * this class serves to implement the orderedListADT, whick is to implemet
+ * the add function to the linkedList class.
+ * @author Patrick Thrower
+ * @version 1.0
+ * @param <T>
+ */
+
+public class OrderedLinkedList<T> extends LinkedList<T> implements OrderedListADT<T>{
+
+    @Override
+     /**
+    * Adds the specified element to this list at the proper location
+    *
+    * @param element the element to be added to this list
+    */
+    public void add(T element) {
+    }
+    
+}

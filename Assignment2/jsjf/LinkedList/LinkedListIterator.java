0d2bbc978737f888010e7a94142edaefc369f201
mrjury76 <mrjury76@gmail.com>
2024-02-05 13:46:41 -0800

    fixes braces

--- Assignment2/jsjf/LinkedList/LinkedListIterator.java
+++ Assignment2/jsjf/LinkedList/LinkedListIterator.java
@@ -1,71 +1,59 @@
 package jsjf.LinkedList;
 
 import java.util.Iterator;
-
 import jsjf.LinearNode;
 
-
-
-    public class LinkedListIterator<T> implements Iterator<T>
-    {
+public class LinkedListIterator<T> implements Iterator<T> {
     private int iteratorModCount; // the number of elements in the collection
     private LinearNode<T> current; // the current position
 
     /**
-    * Sets up this iterator using the specified items.
-    *
-    * @param collection the collection the iterator will move over
-    * @param size the integer size of the collection
-    */
-    public LinkedListIterator()
-    {
-    current = head;
-    iteratorModCount = modCount;
+     * Sets up this iterator using the specified items.
+     *
+     * @param collection the collection the iterator will move over
+     * @param size       the integer size of the collection
+     */
+    public LinkedListIterator() {
+        current = head;
+        iteratorModCount = modCount;
     }
 
     /**
-    * Returns true if this iterator has at least one more element
-    * to deliver in the iteration.
-    *
-    * @return true if this iterator has at least one more element to deliver
-    * in the iteration
-    * @throws ConcurrentModificationException if the collection has changed
-    * while the iterator is in use
-    */
-    public boolean hasNext() throws ConcurrentModificationException
-    {
-    if (iteratorModCount != modCount)
-    throw new ConcurrentModificationException();
+     * Returns true if this iterator has at least one more element to deliver in the iteration.
+     *
+     * @return true if this iterator has at least one more element to deliver in the iteration
+     * @throws ConcurrentModificationException if the collection has changed while the iterator is in use
+     */
+    public boolean hasNext() throws ConcurrentModificationException {
+        if (iteratorModCount != modCount)
+            throw new ConcurrentModificationException();
 
-    return (current != null);
+        return (current != null);
     }
 
     /**
-    * Returns the next element in the iteration. If there are no
-    * more elements in this iteration, a NoSuchElementException is
-    * thrown.
-    *
-    * @return the next element in the iteration
-    * @throws NoSuchElementException if the iterator is empty
-    */
-    public T next() throws ConcurrentModificationException
-    {
-    if (!hasNext())
-    throw new NoSuchElementException();
+     * Returns the next element in the iteration. If there are no more elements in this iteration,
+     * a NoSuchElementException is thrown.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iterator is empty
+     */
+    public T next() throws ConcurrentModificationException {
+        if (!hasNext())
+            throw new NoSuchElementException();
 
-    T result = current.getElement();
-    current = current.getNext();
-    return result;
+        T result = current.getElement();
+        current = current.getNext();
+        return result;
     }
 
     /**
-    * The remove operation is not supported.
-    *
-    * @throws UnsupportedOperationException if the remove operation is called
-    */
-    public void remove() throws UnsupportedOperationException
-    {
-    throw new UnsupportedOperationException();
+     * The remove operation is not supported.
+     *
+     * @throws UnsupportedOperationException if the remove operation is called
+     */
+    public void remove() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
     }
 
     public int getCount() {
@@ -99,5 +87,4 @@ import jsjf.LinearNode;
     public void setModCount(int modCount) {
         this.modCount = modCount;
     }
-
 }

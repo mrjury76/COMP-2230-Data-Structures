8398f945e2f7ebf5791c8eff6b02025a1790b3d9
mrjury76 <mrjury76@gmail.com>
2024-02-08 00:13:32 -0800

    fleshing out the driver class

--- Assignment2/jsjf/LinkedList/DriverAssignment4.java
+++ Assignment2/jsjf/LinkedList/DriverAssignment4.java
@@ -6,17 +6,32 @@ public class DriverAssignment4 {
 
         OrderedLinkedList<Integer> linkedlist = new OrderedLinkedList<Integer>();
 
-        for(int i = 0; i < 3; i++) {
-            linkedlist.add(10);
-            System.out.println(linkedlist.toString());
-
+        System.out.println("Is empty?: " + linkedlist.isEmpty());
+        for(int i = 3; i >= 0; i--) {
+            linkedlist.add(i);
         }
-        linkedlist.add(10);
-        linkedlist.add(10);
-        System.out.println(linkedlist.size());
+
+        for (int i = 0; i < 3; i++) {
+            linkedlist.add(i+20);
+        }
+
+        System.out.println(linkedlist.toString());
+        System.out.println("Removing last node: " + linkedlist.removeLast());
+        System.out.println("Removing first node: " + linkedlist.removeFirst());
         System.out.println();
-        System.out.println(linkedlist.iterator());
-        System.out.println(linkedlist.isEmpty());
+
+        System.out.println("Does the linked list contain 20?: " + linkedlist.contains(20));
+        System.out.println("Does the linked list contain 3?: " + linkedlist.contains(22));
+        System.out.println(linkedlist.toString());
+        System.out.println("Removing element: " + linkedlist.remove(20));
+        System.out.println();
+
+        System.out.println(linkedlist.toString());
+        System.out.println("Is empty?: " + linkedlist.isEmpty());
+        System.out.println("Size: " + linkedlist.size());
+        System.out.println("first item in list: " + linkedlist.first() + "last item in list: " + linkedlist.last());
+        System.out.printf("Printing the count variable: %d \tAnd the modCount variable: %d", linkedlist.count, linkedlist.modCount);
+        
     }
     
 }

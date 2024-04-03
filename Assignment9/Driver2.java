64a813d64c0a3bb3e194db934f578301e774a8cc
mrjury76 <mrjury76@gmail.com>
2024-04-03 13:38:02 -0700

    small stuff

---
+++ Assignment9/Driver2.java
@@ -0,0 +1,14 @@
+package Assignment9;
+
+import java.util.Set;
+
+
+public class Driver2 {
+    public static void main(String[] args) {
+
+        SetADT set = new SetADT(10);
+        set.add(1);
+        set.add(2);
+        set.toString();
+    }
+}
